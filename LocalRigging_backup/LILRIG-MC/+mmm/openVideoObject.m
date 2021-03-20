f9c979eae8bb2ba41f21403b900c25d113411475
takacsflora <40361594+takacsflora@users.noreply.github.com>
2021-03-20 18:42:54 +0000

    adding microphone and fastfacecam

--- LocalRigging_backup/LILRIG-MC/+mmm/openVideoObject.m
+++ LocalRigging_backup/LILRIG-MC/+mmm/openVideoObject.m
@@ -9,7 +9,7 @@ rigName = rigName(1:end-1); % removing the Line Feed character
 if strcmp(videoID, 'listAll')
     switch rigName
         case 'LILRIG-MC'
-            vidObj = {'eye','face'};
+            vidObj = {'eye','face', 'fastface'};
             return;
         otherwise
             vidObj =  {'none'};
@@ -40,6 +40,32 @@ switch [rigName '_' videoID]
         src.GainMode = 'manual';
         src.Gain = 600;
         src.FrameRate = '30.0000';
+        
+        vidObj.FramesPerTrigger = Inf;
+        vidObj.LoggingMode = 'disk';
+        
+    case 'LILRIG-MC_fastface'
+        
+        % Set camera device name (from imaqhwinfo)
+        cam_DeviceName = 'DMK 23U618';
+        
+        % Set strobe output (native to camera)
+        vidObj = videoinput('tisimaq_r2013_64',cam_DeviceName,'Y800 (640x480)');
+        src = getselectedsource(vidObj);
+        src.Strobe = 'Enable';
+        src.StrobeMode = 'fixed duration';
+        src.StrobeDuration = 10000;
+        src.FrameRate = '60.00';
+        delete(vidObj); clear vidObj src
+        
+        % Set recording properties (with winvideo)
+        vidObj = videoinput('winvideo',cam_DeviceName,'Y800_640x480');
+        src = getselectedsource(vidObj);
+        src.ExposureMode = 'manual';
+        src.Exposure = -6;
+        src.GainMode = 'manual';
+        src.Gain = 600;
+        src.FrameRate = '60.0002';
         
         vidObj.FramesPerTrigger = Inf;
         vidObj.LoggingMode = 'disk';
--- LocalRigging_backup/LILRIG-STIM/+srv/findService.m
+++ LocalRigging_backup/LILRIG-STIM/+srv/findService.m
@@ -8,14 +8,20 @@ function s = findService(id, varargin)
 % 2013-06 CB created
 
 timelineHost = iff(any(strcmp(id, 'timeline')), {'lilrig-timeline'}, {''});
+micHost = iff(any(strcmp(id, 'microphone')), {'lilrig-timeline'}, {''});
+timelinePort = 1001;
+micPort = 1002;
 
-remoteHosts = [timelineHost];
+remoteHosts = [timelineHost, micHost];
+remotePorts = {timelinePort, micPort};
+
 emp = cellfun(@isempty, remoteHosts);
 
 MpepHosts = io.MpepUDPDataHosts(remoteHosts(~emp));
 MpepHosts.ResponseTimeout = 30;
 MpepHosts.Id = 'MPEP-Hosts';
 MpepHosts.Title = 'mPep Data Acquisition Hosts'; % name displayed on startup
+MpepHosts.RemotePorts = remotePorts(~emp);
 MpepHosts.open();
 s = {MpepHosts};
 end
